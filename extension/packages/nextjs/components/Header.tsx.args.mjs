e4d0c4c7ce83d903de8664d716e53e62000ede87
Victor <vick2592@yahoo.com>
2024-11-08 23:52:27 -0500

    Header updated

---
+++ extension/packages/nextjs/components/Header.tsx.args.mjs
@@ -0,0 +1,6 @@
+export const menuIconImports = `import { ArrowsRightLeftIcon } from "@heroicons/react/24/outline";`;
+export const menuObjects = `{
+  label: "Squid",
+  href: "/squid",
+  icon: <ArrowsRightLeftIcon className="h-4 w-4" />,
+}`;
